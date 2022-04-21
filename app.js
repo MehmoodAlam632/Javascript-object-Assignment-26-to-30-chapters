f53e68377d4b0bbbf15b6f1c9fb364c7b57f47dd
mehmood <mehmoodalam60632@gmail.com>
2022-04-21 19:53:41 +0500

    Javascript object Assignment 26 to 30 chapters

---
+++ app.js
@@ -0,0 +1,31 @@
+document.write("<h1>Javascript Object 26 to 30</h1>");
+
+        //________________Question . 01_________________//
+// var user = +prompt("Enter your number");
+// document.write("Number : " +  user + "<br/>");
+// document.write("Round off value : " + Math.round(user) + "<br/>");
+// document.write("Floor value : " + Math.floor(user) + "<br/>");
+// document.write("Ceil value : " + Math.ceil(user) + "<br/>");
+
+        //________________Question . 02_________________//
+
+// var user = +prompt("Enter your number");
+// document.write("Number : " +  user + "<br/>");
+// document.write("Round off value : " + Math.round(user) + "<br/>");
+// document.write("Floor value : " + Math.floor(user) + "<br/>");
+// document.write("Ceil value : " + Math.ceil(user) + "<br/>");
+
+        //________________Question . 03_________________//
+// var user = +prompt("Enter your number");
+// var a = Math.abs(user);
+// document.write("The absolute value of " + user + " is " + a);
+
+        //________________Question . 04_________________//
+// var a = Math.floor(Math.random() * 6);
+// var b = Math.floor(Math.random() * 5);
+// document.write("Random Dice value is : " +  a + "<br/>");
+// document.write("Random Dice value is : " +  b);
+
+        //________________Question . 05_________________//
+        //________________Question . 01_________________//
+
